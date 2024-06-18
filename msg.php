61af08b067e7a3fce3d1124a568475089d4e1f78
root <root@www.haselsberger.at>
2024-06-18 21:26:08 +0200

    alte files entfernt

--- msg.php
+++
@@ -1,8 +0,0 @@
-<?php
-$data = [ 
-'Sender' => 'IT-Abteilung', 
-'Message' => 'Achtung das ist noch ein TEST <b>fettgedruckt</b> geht auch<br>Hier noch weiterer Inhalt<br><h2>Unterüberschrift</h2>Hier noch weiterer Inhalt<br>',
-'Active'=> true];
-header('Access-Control-Allow-Origin: *');
-header('Content-Type: application/json; charset=utf-8;');
-echo json_encode($data);
